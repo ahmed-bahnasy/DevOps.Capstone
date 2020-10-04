999e1954b704b9a0bfd72fa600e9a92ae275c301
ahmed-bahnasy <ahmed.bahnasy.1000@gmail.com>
2020-10-04 05:20:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
-#import click
-
-#@click.command()
-#def hello():
-#    click.echo('Hello World!')
-#if __name__ == '__main__':
-#    hello()
-
-
-
 
 from flask import Flask
 app = Flask(__name__)
@@ -18,4 +8,4 @@ def hello_world():
 	return 'Hello, World!'
 
 if __name__ == "__main__":
-	app.run(host='localhost', port=8000, debug=True)
+	app.run(host='0.0.0.0', port=8000, debug=True)
