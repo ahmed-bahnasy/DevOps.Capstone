1461b47c9d7867def6d4879e082898563d3f3977
Ubuntu <ubuntu@ip-172-31-39-177.us-west-2.compute.internal>
2020-10-04 01:28:41 +0000

    edit app file

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
-import click
+#import click
 
-@click.command()
-def hello():
-    click.echo('Hello World!')
+#@click.command()
+#def hello():
+#    click.echo('Hello World!')
+#if __name__ == '__main__':
+#    hello()
 
-if __name__ == '__main__':
-    hello()
+
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello, World!'
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=80, debug=True)
