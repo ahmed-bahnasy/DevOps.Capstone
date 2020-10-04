ad6cfdadbcbb97bdb376247c634e67b91f28128e
ahmed-bahnasy <ahmed.bahnasy.1000@gmail.com>
2020-10-04 05:23:01 +0200

    Create delete_kubernetes.sh

---
+++ delete_kubernetes.sh
@@ -0,0 +1,3 @@
+#The first all means the common resource kinds (pods, replicasets, deployments, ...)
+#The second --all means to select all resources of the selected kinds
+kubectl delete all --all --all-namespaces
